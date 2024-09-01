8a4cecc0a97420f5a66331afe131b8549e2da3be
Jerrod Estell <jrodmath1@gmail.com>
2024-09-01 09:28:45 -0700

    changed run.py to app.py to avoid flask conflicts

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "<html><body><h1>EveryVoter app landing page</h1></body></html>\n"
+
+if __name__ == "__main__":
+    app.run(debug=True)
