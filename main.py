7690692ed08c56584536a2b926ee8d3b135eb85b
Jerrod Estell <jrodmath1@gmail.com>
2024-09-01 10:08:01 -0700

    restructure | basic templates added

---
+++ main.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/vote")
+def vote():
+    return render_template("vote.html")
+
+@app.route("/admin")
+def admin():
+    return render_template("admin.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
